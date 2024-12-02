72d0fc9f9fdad6a1dbdba87347bdc0f8872eb6d0
somnath <somnathbm0@gmail.com>
2024-12-02 21:52:29 +0530

    admission creation logic added

--- utils/helper.py
+++ utils/helper.py
@@ -12,40 +12,44 @@ def create_new_emg_admission(patient_id: str):
     # check patient info from the patient mgmt database
     # if record exists, then proceed
     # otherwise notify the reason in the response
-    db_uri: str = os.environ.get("DB_URI")
     db_name: str = os.environ.get("DB_NAME")
     emg_dept_collection: str = os.environ.get("EMG_DEPT_COLLECTION")
     pmgmt_collection: str = os.environ.get("PMGMT_COLLECTION")
 
     lookup_result = lookup_patient(patient_id, db_name, pmgmt_collection)
-    print(lookup_result)
-    if not lookup_result:
-      return {"error": True, "reason": "patient not found"}
+    # print(lookup_result)
+    if lookup_result is None:
+      return {"error": True, "reason": f"No patient found with patient id {patient_id}"}
     
-    # medical_data = lookup_result["medical_info"]
-    # patient_basic_data = lookup_result["basic_info"]
+    medical_data = lookup_result["medical_info"]
+    patient_basic_data = lookup_result["basic_info"]
 
-    # # create emergency admission
-    # # prepare the payload
-    # payload = {
-    #   "admission_id": uuid4(),
-    #   "admitted_on": datetime.now(UTC),
-    #   "patient_id": patient_id,
-    #   "patient_name": patient_basic_data["name"],
-    #   "department": medical_data["department"],
-    #   "history": medical_data["history"],
-    #   "illness_primary": medical_data["illness_primary"]
-    # }
+    # create emergency admission
+    # prepare the payload
+    payload = {
+      "admission_id": str(uuid4()),
+      "admitted_on": datetime.now(UTC),
+      "patient_id": patient_id,
+      "patient_name": patient_basic_data["name"],
+      "department": medical_data["department"],
+      "history": medical_data["history"],
+      "illness_primary": medical_data["illness_primary"]
+    }
 
-    # insert_ok = client[emg_dept_collection].insert_one(payload).acknowledged
-    # if not insert_ok:
-    #   return {"error": True, "reason": "db insertion failure"}
+    insert_ok = client[db_name][emg_dept_collection].insert_one(payload).acknowledged
+    if not insert_ok:
+      return {"error": True, "reason": "db insertion failure"}
     return {"error": False, "status": "ok", "data": lookup_result}
   except Exception as err:
     raise err
   
 
+# Lookup patient info
 def lookup_patient(patient_id: str, db_name: str, patient_mgmt_collection_name: str):
-  patient_mgmt_collection = client[db_name][patient_mgmt_collection_name]
-  result = patient_mgmt_collection.find_one({"medical_info.patientId": patient_id}, {"_id": 0})
-  return result
+  """Lookup patient info"""
+  try:
+    patient_mgmt_collection = client[db_name][patient_mgmt_collection_name]
+    result = patient_mgmt_collection.find_one({"medical_info.patientId": patient_id}, {"_id": 0})
+    return result
+  except Exception as err:
+    raise err
